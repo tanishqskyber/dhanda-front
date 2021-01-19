5cd7f7ef955cccf3f31f4255ef717527726e9b1b
tanishqskyber <tanishq@skyber.cloud>
2021-01-19 15:03:38 +0530

    css changes done

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,9 +2,9 @@ export class Config{
     baseUrl:string
 
     constructor(){
-        this.baseUrl = "http://fc7e9d8d2bcd.ngrok.io/"  //development
+        // this.baseUrl = "http://fc7e9d8d2bcd.ngrok.io/"  //development
         //this.baseUrl = "http://13.127.199.188:4001/"  //production
-        // this.baseUrl = "http://3.7.70.161:4001/"  //tes
+        this.baseUrl = "http://3.7.70.161:4001/"  //tes
     }
 
     getBaseURL(){
