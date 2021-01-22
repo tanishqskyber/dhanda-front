dee61ff526449636038f99f72738315108d2494c
Sunit121 <sunit.vadhia@gmail.com>
2021-01-22 13:50:46 +0530

    css changes done

--- src/app/config.ts
+++ src/app/config.ts
@@ -3,8 +3,8 @@ export class Config{
 
     constructor(){
        //this.baseUrl = "http://988722fcdf36.ngrok.io/"  //development
-        this.baseUrl = "http://13.127.199.188:4001/"  //production
-        //  this.baseUrl = "http://3.7.70.161:4001/"  //tes
+        // this.baseUrl = "http://13.127.199.188:4001/"  //production
+         this.baseUrl = "http://3.7.70.161:4001/"  //tes
     }
 
     getBaseURL(){
